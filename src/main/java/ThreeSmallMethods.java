836e86ebc8db9ec4f77c27ba6fff70ce27f4d552
KatheRojasO <kathe.rojaso@gmail.com>
2024-09-14 14:42:04 +0200

    Implement logic to check if PN second to last number is odd or even

--- src/main/java/ThreeSmallMethods.java
+++ src/main/java/ThreeSmallMethods.java
@@ -1,6 +1,7 @@
 public class ThreeSmallMethods {
 
     public boolean isYoung(int age) {
+        //refactor
         if (age <= 30) {
             return true;
         } else {
@@ -10,11 +11,20 @@ public class ThreeSmallMethods {
 
     public boolean hasLegalGenderManInSweden(String personalNumber) {
         int personalNumberLength = 10;
+        int index = personalNumber.length() - 2;
+        char secondToLastNumberChar = personalNumber.charAt(index);
+        int secondToLastNumber = Integer.parseInt(String.valueOf(secondToLastNumberChar));
 
+        //Refactor
         if (personalNumber.length() < personalNumberLength || personalNumber.length() > personalNumberLength) {
             return false;
         }
-        return false;
+
+        if (secondToLastNumber % 2 == 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public int firstTwoDigitInteger(int[] numbers) {
